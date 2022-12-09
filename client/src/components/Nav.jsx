8adbac0bc73537cd400fa7123a80109d61a5c3c6
Malik Washington <malikwashington@gmail.com>
2022-12-08 21:53:16 -0500

    just getting started, got my nav bar started

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <ul className='nav-container'>
+      <li className='nav-link'>About</li>
+      <li className='nav-link'>Projects</li>
+      <li className='nav-link'>Resume</li>
+    </ul>
+  )
+}
