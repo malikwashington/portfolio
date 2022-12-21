b7e96a2a7d63b0239fb97d45f411f398f9292016
Malik Washington <malikwashington@gmail.com>
2022-12-21 13:40:11 -0500

    about page

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
     <div>
-      <h1>About Malik</h1>
-      <p></p>
+      <h1>About</h1>
+      <p>
+        Hi, My name is Malik, I'm from Harlem, USA. I am a Software Engineer
+        with a passion for solving problems, both with computers and with the
+        most complex things we know: people.{" "}
+      </p>
+
+      <p>
+        I have a background of technology sales, technical sales support and
+        Information Technology services. Outside of my traditional technical
+        experience, I also bring to the table a 15+ year career as an
+        Entrepreneur and Professional Musician.
+      </p>
+      <p>
+        Entrepreneurship and performing around the world have given me the
+        opportunity to negotiate different modes of thought, action and
+        effective leadership styles. I bring a worldly, unique and diverse
+        perspective that challenges stagnation, promotes creativity, and
+        produces practical, effective solutions for real-world business
+        challenges.
+      </p>
     </div>
-  )
+  );
 }
