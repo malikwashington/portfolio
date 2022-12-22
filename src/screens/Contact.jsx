e4b3cfdd614a33285a62b429082e86ddb301a97a
Malik Washington <malikwashington@gmail.com>
2022-12-22 13:38:21 -0500

    added projects and resume link

--- src/screens/Contact.jsx
+++ src/screens/Contact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Git from '../assets/git.png'
-import aedIn from '../assets/linkedin.png'
+import LinkedIn from '../assets/linkedin.png'
 import Mail from '../assets/email.png'
 import { a } from "react-router-dom";
 import "./styles/Screens.css";
@@ -8,17 +8,17 @@ import "./styles/Screens.css";
 export default function Contact() {
   return (
     <>
-      <h2>You Can Contact Me Through These Channels</h2>
+      <h2>You Can Follow Me On My Journey Through These Channels:</h2>
       <div className="icon-container">
         <a href="https://github.com/malikwashington" target='_blank'>
           <img className="contact-image" id="git" src={Git} />
         </a>
         <a href="https://www.linkedin.com/in/malikwashington/" target='_blank'>
-          <img className="contact-image" id="aedin" src={aedIn} />
+          <img className="contact-image" id="aedin" src={LinkedIn} />
         </a>
-        <a href="https://www.google.com/" target='_blank'>
+        {/* <a href="https://www.google.com/" target='_blank'>
           <img className="contact-image" id="email" src={Mail} />
-        </a>
+        </a> */}
       </div>
     </>
   );
--- src/screens/Resume.jsx
+++ src/screens/Resume.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import ResumePDF from "../assets/Resume.pdf";
+import { useState } from "react";
+import { Document, Page } from "react-pdf";
+import "./styles/Screens.css";
 
 export default function Resume() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
   return (
-    <div>Resume</div>
-  )
+    <div>
+      <Document file={ResumePDF} onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
+      <a id="resume" href={ResumePDF} target="_blank" rel="noopener noreferrer">
+        Click here for a PDF of my resume
+      </a>
+    </div>
+  );
 }
