6effe9ed3a38f44dd77cf19310d534d2fe629cee
Malik Washington <malikwashington@gmail.com>
2022-12-22 19:04:05 -0500

    changed the resume link to open pdf in a new window. got the pdf to show in the resume endpoint. More work needed to get up to snuff

--- src/screens/Resume.jsx
+++ src/screens/Resume.jsx
@@ -14,13 +14,21 @@ export default function Resume() {
 
   return (
     <div>
-      <Document file={ResumePDF} onLoadSuccess={onDocumentLoadSuccess}>
+      <Document
+        file={ResumePDF}
+        onLoadSuccess={onDocumentLoadSuccess}
+        options={{ workerSrc: "pdf.worker.js" }}
+      >
         <Page pageNumber={pageNumber} />
       </Document>
       <p>
         Page {pageNumber} of {numPages}
       </p>
-      <a id="resume" href={ResumePDF} target="_blank" rel="noopener noreferrer">
+      <a
+        id="resume"
+        href={ResumePDF}
+        target="_blank"
+        rel="noopener noreferrer">
         Click here for a PDF of my resume
       </a>
     </div>
